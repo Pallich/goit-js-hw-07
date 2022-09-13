22d7596332ba85863e33fa7557c1581e43590741
Diachenko Oleksandr <palllich@gmail.com>
2022-09-13 12:24:43 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,12 +25,15 @@ function showFullImage(evt) {
   if (evt.target.nodeName !== "IMG") {
     return;
   }
+
+  preventDefault();
   console.log(evt.target);
   //   const targetedImgUrl = evt.target.dataset.source;
   //   const instance = basicLightbox.create(`
-  //     <img src="${targetedImgUrl}" width="800" height="600">
-  // `);
+  //       <img src="${targetedImgUrl}" width="800" height="600">
+  //   `);
 
   //   instance.show();
+
   //   evt.src = "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg";
 }
