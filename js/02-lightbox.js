1c4fa73026fba4cacc0b7a68299696051429bda0
Diachenko Oleksandr <palllich@gmail.com>
2022-09-14 00:42:41 +0300

    make 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDestination = document.querySelector(".gallery");
+
+// galleryDestination.addEventListener("click", showFullImage);
+
+const createGallery = createImage(galleryItems);
+// galleryDestination.insertAdjacentHTML("beforeend", createGallery);
+galleryDestination.innerHTML = createGallery;
+
+function createImage(galleryItems) {
+  return galleryItems
+    .map((image) => {
+      return `
+      <a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+`;
+    })
+    .join(" ");
+}
+
+new SimpleLightbox(".gallery a", {
+  sourceAttr: "href",
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  loop: true,
+});
